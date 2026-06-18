ee91c79cf9d978dba199ac8fbc7e719362086cb4
PRITHVI ANAND <43229037+prithvianand@users.noreply.github.com>
2026-06-18 23:41:33 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # count_mapping = []
+        # for i in nums:
+        #     if i not in count_mapping:
+        #         count_mapping.append(i)
+        #     else:
+        #         return True
+        # return False
+        # seen = set()
+        # for i in nums:
+        #     if i in seen:
+        #         return True
+        #     seen.add(i)
+        # return False
+        
+        return len(nums) != len(set(nums))
+
+        
