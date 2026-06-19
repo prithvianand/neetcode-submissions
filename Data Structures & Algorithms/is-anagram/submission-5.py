1c7b90fd866eef6396f1d0a9fe5d31fb48715222
PRITHVI ANAND <43229037+prithvianand@users.noreply.github.com>
2026-06-19 12:58:16 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        # mapping_s = {}
+        # mapping_t = {}
+        # for char in s:
+        #     if char not in mapping_s:
+        #         mapping_s[char] = 1
+        #     else:
+        #         mapping_s[char] += 1
+
+        # for char in t:
+        #     if char not in mapping_t:
+        #         mapping_t[char] = 1
+        #     else:
+        #         mapping_t[char] += 1
+        # return mapping_s == mapping_t
+        # counter = {}
+        # for char in s:
+        #     counter[char] = counter.get(char,0) + 1
+        
+        # for char in t:
+        #     counter[char] = counter.get(char,0) - 1
+        
+        # for char_count in counter.values():
+        #     if char_count != 0:
+        #         return False
+
+        # return True
+
+        dict_string = set(s)
+        for char in dict_string:
+            if s.count(char) != t.count(char):
+                return False
+        return True
+
+
+
+
+        
+
+        
